9a5fd5fc7cf11715bd9330faf5d6b695a34f3b1c
burhan1997 <burhanelaldi97@gmail.com>
2024-04-09 13:14:54 +0200

    upload photos from your device functinality

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
-const imageDownloader = require('image-downloader');
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs");
 require("dotenv").config();
 const app = express();
 
@@ -14,7 +16,7 @@ const jwtSecret = "sdfsdaf534fghg34f5gf43uiytyui";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname+'/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -22,7 +24,9 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, {
+  ssl: true, // Explicitly enable SSL/TLS
+});
 
 app.get("/test", (req, res) => {
   res.json("test ok");
@@ -80,10 +84,10 @@ app.get("/profile", (req, res) => {
   }
 });
 
-app.post('/logout', (req, res)=>{
-  res.cookie('token', '').json(true);
-})
-console.log(__dirname)
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+console.log(__dirname);
 app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = "photo" + Date.now() + ".jpg";
@@ -104,5 +108,19 @@ app.post("/upload-by-link", async (req, res) => {
   }
 });
 
+const photosMiddleware = multer({ dest: "uploads/" });
+app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath);
+    uploadedFiles.push(newPath.replace("uploads/", ""));
+  }
+  res.json(uploadedFiles);
+});
 
 app.listen(4000);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -44,7 +44,22 @@ async function addPhotoByLink(ev){
   });
   setPhotoLink('');
 }
-
+  function uploadPhoto(ev){
+  const files = ev.target.files;
+  const data = new FormData();
+  for(let i = 0; i< files.length; i++){
+  data.append("photos", files[i]);
+  }
+ 
+  axios.post('/upload', data, {
+    headers: {'Content-type':'multipart/form-data'}
+  }).then(response => {
+    const {data:filenames} = response;
+    setAddedPhotos((prev) => {
+      return [...prev, ...filenames ];
+    });
+  })
+}
   return (
     <div>
       {action !== "new" && (
@@ -107,18 +122,21 @@ async function addPhotoByLink(ev){
                 Add&nbsp;photo
               </button>
             </div>
+
             <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhotos.length > 0 &&
                 addedPhotos.map((link, index) => (
-                  <div key={index}>
-                    <img className="rounded-2xl"
+                  <div className="h-32 flex" key={index}>
+                    <img
+                      className="rounded-2xl w-full object-cover"
                       src={`http://localhost:4000/uploads/${link}`}
                       alt={`Image ${index}`}
                     />
                   </div>
                 ))}
 
-              <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text=gray-600">
+              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text=gray-600">
+                <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -134,7 +152,7 @@ async function addPhotoByLink(ev){
                   />
                 </svg>
                 Upload
-              </button>
+              </label>
             </div>
             {preInput("Description", "Description of the place")}
 
